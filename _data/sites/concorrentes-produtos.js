802aa01c12383155edd582dc5fe8bf1a526974e1
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-05-04 09:36:09 -0300

    conc prod interval

--- _data/sites/concorrentes-produtos.js
+++ _data/sites/concorrentes-produtos.js
@@ -6,7 +6,7 @@ module.exports={
   // skip: !process.env.CONTEXT||process.env.SITE_NAME!=="speedlify",
   skip: false,
   options: {
-    frequency: 1, // 11h, 30m
+    frequency: 60*11+30, // 11h, 30m
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
